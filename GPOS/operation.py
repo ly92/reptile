7ed04bed0e3408e37401f8cb2a13045123abcfff
ly <1364757394@qq.com>
2018-08-15 18:21:36 +0800

    123

--- GPOS/operation.py
+++ GPOS/operation.py
@@ -38,24 +38,30 @@ info = Operation()
 # for parts in partses:
 #     print(parts['machine_id'])
 
-partses = info.dataManager.select_all("select * from qxf_kfparts WHERE id>101086")
-for parts in partses:
-    sql = '''UPDATE qxf_kfparts SET parts_id = %d WHERE id = %d''' % (parts['id'],parts['id'])
-    result = info.dataManager.op_sql(sql)
-    print(result)
-
-
-# partses = info.dataManager.select_all("select * from qxf_kfpartsmachine")
+# partses = info.dataManager.select_all("select * from qxf_kfparts WHERE id>101086")
 # for parts in partses:
-#     machines = info.dataManager.select_all("select * from qxf_kfmachine WHERE machine_url='%s'"%(parts['machine_url']))
-#     id_arr = []
-#     for machine in machines:
-#         id_arr.append(str(machine['machine_id']))
-#     ids = ','.join(id_arr)
-#     sql = '''UPDATE qxf_kfpartsmachine SET machine_id = %s WHERE id = %d''' % (ids, parts['id'])
-#     # print(sql)
+#     sql = '''UPDATE qxf_kfparts SET parts_id = %d WHERE id = %d''' % (parts['id'],parts['id'])
 #     result = info.dataManager.op_sql(sql)
 #     print(result)
+
+
+# partses = info.dataManager.select_all("select * from qxf_kfpartsmachine WHERE machine_id=0")
+# for parts in partses:
+#     machines = info.dataManager.select_all("select * from qxf_kfmachine WHERE machine_url='%s'"%(parts['machine_url']))
+#     if len(machines) == 1:
+#         machine = machines[0]
+#         sql = '''UPDATE qxf_kfpartsmachine SET machine_id = %d WHERE id = %d''' % (machine['machine_id'], parts['id'])
+#         result = info.dataManager.op_sql(sql)
+#         if not result:
+#             sql2 = '''insert into temp(part_id,pm_id) values(%d,%d)''' % (parts['parts_id'],parts['id'])
+#             info.dataManager.op_sql(sql2)
+#     else:
+#         sql3 = '''insert into temp(part_id,pm_id) values(%d,%d)''' % (parts['parts_id'], parts['id'])
+#         info.dataManager.op_sql(sql3)
+#     # id_arr = []
+#     # for machine in machines:
+#     #     id_arr.append(str(machine['machine_id']))
+#     # ids = ','.join(id_arr)
 #     print('------------------')
 
 
